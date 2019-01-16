551bb578a33947715724fa2c50e6d13c2e6c8946
rasskazshik <rasskazshik@gmail.com>
2019-01-16 10:13:51 +0300

    add favicon

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once 'scriptsPHP/userAuth/getUserInfo.php';
     <meta charset="utf-8" />
     <title>Пятерочка - оценка качества работы отделений</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" media="screen" href="styles/bootstrap.min.css" />
     <link defer rel="stylesheet" type="text/css" media="screen" href="styles/main.css" />
     <!--фреймворк jQuery-->
